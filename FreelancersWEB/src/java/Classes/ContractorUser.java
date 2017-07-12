6b9c197aad7042abfbde79f275353b567c63483a
Shiroyasha <emilio.rojas@ucrso.info>
2017-07-11 23:33:07 -0600

    Revert "Contractor"
    This reverts commit b9f8d044d1c73613dcd4b5006cc4fe60ccd7b26a.

--- FreelancersWEB/src/java/Classes/ContractorUser.java
+++ FreelancersWEB/src/java/Classes/ContractorUser.java
@@ -20,11 +20,15 @@ public class ContractorUser extends User {
     private String description;
     private double price;
     private ArrayList<Request> requestList;
-
+<<<<<<< HEAD
     private ArrayList<String> jobList;
-
+<<<<<<< HEAD
+=======
     private ArrayList<String> jobsList;
 
+>>>>>>> Commit
+=======
+>>>>>>> origin/master
     public ContractorUser() {
     }
 
